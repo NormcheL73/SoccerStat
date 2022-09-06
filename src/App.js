dbe5195833ce979ead5c010cdc3c06a25c54404a
normchel <normchel73@gmail.com>
2022-09-06 19:46:19 +0400

    Mui added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css"
+import Button from "@mui/material/Button"
+import ezhik from "./images/ezhik.jpg"
 
 function App() {
   return (
     <div className="App">
       <h1>ХаюхайТест</h1>
+      <img src={ezhik} alt="Картинка ёжика" />
+      <Button variant="outlined">Test mui</Button>
     </div>
   )
 }
