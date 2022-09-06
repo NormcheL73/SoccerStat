ef49a2fda0eaab6a9b20c44cbaba70b4071c49cd
normchel <normchel73@gmail.com>
2022-09-06 16:43:17 +0400

    ESLint + Prettier plugins & config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <h1>Хаюхай</h1>
+      <h1>ХаюхайТест</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
