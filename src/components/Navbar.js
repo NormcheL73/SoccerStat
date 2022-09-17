9e16ea28c4d77ff7ca2be71c56bc1358cf427c4a
normchel <normchel73@gmail.com>
2022-09-17 19:24:12 +0400

    Interactivity to navbar icon added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,40 @@
 import { SportsSoccer } from "@mui/icons-material"
-import { AppBar, Tab, Tabs, Toolbar } from "@mui/material"
+import { AppBar, IconButton, Tab, Tabs } from "@mui/material"
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
 function Navbar() {
   const [value, setValue] = useState(0)
+
   const handleChange = (event, newValue) => {
     setValue(newValue)
+    console.log(value)
+  }
+
+  const handleClick = () => {
+    setValue(0)
   }
 
   return (
-    <AppBar position="static" sx={{ height: 56 }}>
-      <Toolbar>
-        <SportsSoccer fontSize="large" />
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          textColor="inherit"
-          TabIndicatorProps={{ sx: { bgcolor: "white" } }}
-          sx={{ ml: "1rem" }}
-        >
-          <Tab label="Лиги" to="/SoccerStat" component={Link} />
-          <Tab label="Команды" to="/teams" component={Link} />
-        </Tabs>
-      </Toolbar>
+    <AppBar position="static" sx={{ flexDirection: "row" }}>
+      <IconButton
+        component={Link}
+        to="/SoccerStat"
+        onClick={handleClick}
+        sx={{ ml: "1rem" }}
+      >
+        <SportsSoccer fontSize="large" sx={{ color: "white" }} />
+      </IconButton>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        textColor="inherit"
+        TabIndicatorProps={{ sx: { bgcolor: "white" } }}
+        sx={{ ml: "1rem" }}
+      >
+        <Tab label="Лиги" to="/SoccerStat" component={Link} />
+        <Tab label="Команды" to="/teams" component={Link} />
+      </Tabs>
     </AppBar>
   )
 }
